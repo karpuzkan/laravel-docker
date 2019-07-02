fae563897e94d0b40e3d01fc7d0540c9c1817b39
Faik TOFUR <faiktofur@gmail.com>
2019-07-02 22:30:12 +0300

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,4 @@
 cp ./apache/conf/apache2.conf.example ./apache/conf/apache2.conf
 cp ./apache/sites-enabled/000-default.conf.example ./apache/sites-enabled/000-default.conf
+cp ./docker-compose.yml.example ./docker-compose.yml
+cp ./docker-compose.testing.yml.example ./docker-compose.testing.yml
