2234296fa636d44b48bf88339e88b31c6aba6198
Faik TOFUR <faiktofur@gmail.com>
2019-07-25 14:53:37 +0300

    example name changed

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 cp ./apache/conf/apache2.conf.example ./apache/conf/apache2.conf
 cp ./apache/sites-enabled/000-default.conf.example ./apache/sites-enabled/000-default.conf
-cp ./docker-compose.yml.example ./docker-compose.yml
-cp ./docker-compose.testing.yml.example ./docker-compose.testing.yml
+cp ./example.docker-compose.yml ./docker-compose.yml
+cp ./example.docker-compose.testing.yml ./docker-compose.testing.yml
